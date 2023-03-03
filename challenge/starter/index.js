42c7e8abe66e65ec12bf50c35826fc9c399ad154
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-03-03 15:00:57 +0000

    createEngineer function updated

--- challenge/starter/index.js
+++ challenge/starter/index.js
@@ -13,6 +13,40 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+function createEngineer(team) {
+    inquirer.prompt([
+        // Engineer name
+        // Engineer id
+        // Engineer email
+        // Engineer GitHub username
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the engineer's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email?",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's github?",
+        }
+    ]).then((engineerDetails) => {
+        // Initialise Engineer class to create Manager object
+         const engineer = new Engineer(engineerDetails.name, engineerDetails.id, engineerDetails.email, engineerDetails.githubUsername)
+        team.push(engineer);
+        createTeam(team); // at this point we add an engineer to the team array
+    });
+ 
+
 function start() {
     const team = []; // array of Employee objects (array of Manager, or Engineers, or Interns)
     // Employee can be Manager, Engineer, or Intern
